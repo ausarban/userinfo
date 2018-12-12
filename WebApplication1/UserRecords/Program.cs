a64fb27b29fe0e10a167c29e30a2383d49c5dad7
Austin Arban <aarban@codeauthority.com>
2018-12-12 10:49:13 -0600

    comments added

---
+++ WebApplication1/UserRecords/Program.cs
@@ -0,0 +1,151 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace UserRecords
+{
+    class Program
+    {
+        private const string baseUrl = "https://jsonplaceholder.typicode.com/";
+        private static readonly IList<int> validIds = new ReadOnlyCollection<int>(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });  
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Console.WriteLine("Please enter a user id (1-10) to retrieve user records...");
+
+            int id = 0;
+            
+            try
+            {
+                while (!validIds.Contains(id))
+                {
+                    if (!int.TryParse(Console.ReadLine(), out id))
+                        Console.WriteLine("Please enter a valid user id between 1 and 10");
+                }
+
+                List<User> user = program.GetUser(id).GetAwaiter().GetResult();
+
+                Console.WriteLine(string.Format("retrieving records for user id {0}...", id));
+
+                Console.WriteLine();
+
+                List<UserRecord> users = program.GetUserRecords(user).GetAwaiter().GetResult();
+
+                for (var i = 0; i < users.Count; i++)
+                    Console.WriteLine(string.Format(" {0} - {1} ", users[i].Name, users[i].Body));
+
+                Console.ReadLine();
+
+            }catch(Exception ex)
+            {
+                Console.WriteLine(string.Format("Something went wrong... Application is stopping for...{0}", ex.Message)); //We would normally log the exception here and handle it accordingly.
+                Task.Delay(5000);
+                return;
+            }
+           
+        }
+
+        public async Task<List<User>> GetUser(int id)
+        {            
+            try
+            {
+                var client = new HttpClient(); //we wont need any headers here
+                client.BaseAddress = new Uri(baseUrl + "users?id=" + id); // pass the parameter to get our user record 
+                var response = await client.GetAsync(client.BaseAddress); 
+
+                response.EnsureSuccessStatusCode(); //throws an exception if API isn't happy with request.
+
+                string record = await response.Content.ReadAsStringAsync();
+                var user = JsonConvert.DeserializeObject<List<User>>(record); //Json is coming in as an array so let's deserialize to a list.
+
+                client.Dispose(); // dispose the resources after our client is done here. 
+                return user;
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine(string.Format("An Exception has occured, Application is stopping with... {0}", e.Message));
+                return null;
+            }
+        }
+
+        public async Task<List<UserRecord>> GetUserRecords(List<User> user)
+        {
+            var records = new List<UserRecord>();
+
+            try
+            {
+                var client = new HttpClient();
+
+                client.BaseAddress = new Uri(baseUrl + "posts?userid=" + user[0].ID);
+                var response = await client.GetAsync(client.BaseAddress);
+
+                response.EnsureSuccessStatusCode();
+
+                var userRecords = await response.Content.ReadAsStringAsync();
+
+                records = JsonConvert.DeserializeObject<List<UserRecord>>(userRecords);
+
+                for (var i = 0; i < records.Count; i++)
+                    records[i].Name = user[0].Name;
+
+                client.Dispose();
+                return records;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(string.Format("An Exception has occured, Application is stopping with... {0}", e.Message));
+
+                return null;
+            }
+
+        }
+    }
+ 
+
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public Address Address { get; set; }
+        public string Phone { get; set; }
+        public string Website { get; set; }
+        public Company Company { get; set; }
+
+    }
+    public class Address
+    {
+        public string Street { get; set; }
+        public string Suite { get; set; }
+        public string City { get; set; }
+        public string Zipcode { get; set; }
+        public Geo Geo { get; set; }
+
+    }
+    public class Geo
+    {
+        public decimal Lat { get; set; }
+        public decimal Lng { get; set; }
+    }
+    public class Company
+    {
+        public string Name { get; set; }
+        public string CatchPhrase { get; set; }
+        public string Bs { get; set; }
+
+    }
+    public class UserRecord : User
+    {
+        public int UserID { get; set; }
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+
+}
--- WebApplication1/WebApplication1/Controllers/ValuesController.cs
+++ WebApplication1/WebApplication1/Controllers/ValuesController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace WebApplication1.Controllers
 {
@@ -10,36 +12,84 @@ namespace WebApplication1.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        private const string baseUrl = "https://jsonplaceholder.typicode.com/";
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public async Task<List<UserRecord>> GetUserRecords(List<User> user)
         {
-            return "value";
-        }
+            var records = new List<UserRecord>();
 
+            try
+            {
+                var client = new HttpClient();
+
+                client.BaseAddress = new Uri(baseUrl + "posts?userid=" + user[0].ID);
+                var response = await client.GetAsync(client.BaseAddress);
+
+                response.EnsureSuccessStatusCode();
+
+                var userRecords = await response.Content.ReadAsStringAsync();
+
+                records = JsonConvert.DeserializeObject<List<UserRecord>>(userRecords);
+
+                client.Dispose();
+                return records;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(string.Format("An Exception has occured, Application is stopping with... {0}", e.Message));
+               
+                return null;
+            }
+
+        }
         // POST api/values
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+    }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+    public class User : UserRecord
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public Address Address { get; set; }
+        public string Phone { get; set; }
+        public string Website { get; set; }
+        public Company Company { get; set; }
+
+    }
+    public class Address
+    {
+        public string Street { get; set; }
+        public string Suite { get; set; }
+        public string City { get; set; }
+        public string Zipcode { get; set; }
+        public Geo Geo { get; set; }
+
+    }
+    public class Geo
+    {
+        public decimal Lat { get; set; }
+        public decimal Lng { get; set; }
+    }
+    public class Company
+    {
+        public string Name { get; set; }
+        public string CatchPhrase { get; set; }
+        public string Bs { get; set; }
+
+    }
+    public class UserRecord
+    {
+        public int UserID { get; set; }
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
     }
 }
